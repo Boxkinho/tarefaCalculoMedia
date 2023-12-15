8e9570f713970a9bdf1847bfd73162681e9f1b52
boxkinho <jbmmf@yahoo.com.br>
2023-12-14 22:07:19 -0300

    Exercicio M8

---
+++ TarefaMedia/src/m8/MediaNotas.java
@@ -0,0 +1,18 @@
+package m8;
+
+public class MediaNotas {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 9;
+		int nota2 = 5;
+		int nota3 = 7;
+		int nota4 = 8;
+		
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("A média das notas é igual a: " + media);
+
+	}
+
+}
